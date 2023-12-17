cb2078e436c083f8fa06ba6023179cf314eb8022
Shidqi Naufal Musyaffa <snaufalm@gmail.com>
2023-12-17 16:09:46 +0700

    i2c scan

--- src/main.cpp
+++ src/main.cpp
@@ -111,6 +111,40 @@ void uplink(String payload){
 
 LiquidCrystal_I2C lcd(reg_LCD, lcdColumns, lcdRows);  
 
+void i2cscan(){
+  byte error, address;
+  int Devices;
+  Serial.println("Scanning...");
+  Devices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+
+  Wire.beginTransmission(address);
+  error = Wire.endTransmission();
+  if (error == 0)
+  {
+  Serial.print("I2C device found at address 0x");
+  if (address<16)
+  Serial.print("0");
+  Serial.print(address,HEX);
+  Serial.println("  !");
+  Devices++;
+  }
+  else if (error==4)
+  {
+  Serial.print("Unknown error at address 0x");
+  if (address<16)
+  Serial.print("0");
+  Serial.println(address,HEX);
+  }
+  }
+  if (Devices == 0)
+  Serial.println("No I2C devices found\n");
+  else
+  Serial.println("done\n");
+  delay(5000);          
+
+}
 void initLCD(){
   lcd.init();              
   lcd.backlight();
@@ -199,5 +233,10 @@ void test(){
 }
 
 void loop() {
-  test();
+  i2cscan();
+  while (1)
+  {
+    test();
+  }
+  
 }
